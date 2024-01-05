5a10fb2044f99bacd6b00b6e357dff9b6b4d973e
juniorsundarcasual <che.naasam@gmail.com>
2024-01-05 11:27:13 +0400

    Fixed some unlabelled keybindings

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -40,10 +40,10 @@ return {
 
 			-- Global mappings.
 			-- See `:help vim.diagnostic.*` for documentation on any of the below functions
-			vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
-			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
-			vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
-			vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
+			-- vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
+			-- vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+			-- vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+			-- vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
 
 			-- Use LspAttach autocommand to only map the following keys
 			-- after the language server attaches to the current buffer
@@ -66,13 +66,13 @@ return {
 					vim.keymap.set("n", "<space>wl", function()
 						print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
 					end, opts)
-					vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
-					vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+					-- vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+					-- vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
 					vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
 					vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-					vim.keymap.set("n", "<space>f", function()
-						vim.lsp.buf.format({ async = true })
-					end, opts)
+					-- vim.keymap.set("n", "<space>f", function()
+					--	vim.lsp.buf.format({ async = true })
+					-- end, opts)
 				end,
 			})
 		end,
