4323b41bc2a754380bc149b8ed29205154368123
juniorsundarcasual <che.naasam@gmail.com>
2024-01-09 11:26:57 +0400

    Updated the lsp and whichkey position

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -52,7 +52,7 @@ return {
                 height = { min = 4, max = 25 },                                  -- min and max height of the columns
                 width = { min = 20, max = 50 },                                  -- min and max width of the columns
                 spacing = 3,                                                     -- spacing between columns
-                align = "left",                                                  -- align columns left, center or right
+                align = "center",                                                -- align columns left, center or right
             },
             ignore_missing = false,                                              -- enable this to hide mappings for which you didn't specify a label
             hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
@@ -93,11 +93,13 @@ return {
             p = { "<cmd>lua require('telescope').extensions.projects.projects()<cr>", "Projects" },
             l = { "<cmd>Lazy<cr>", "Lazy" },
             m = { "<cmd>Mason<cr>", "Mason" },
+            -- Autocompletion
             A = {
                 name = "Autocompletion",
-                e = {"<cmd>lua require 'cmp'.setup{ enabled = true }<cr>", "Enabled"},
-                d = {"<cmd>lua require 'cmp'.setup{ enabled = false }<cr>", "Disabled"},
+                e = { "<cmd>lua require 'cmp'.setup{ enabled = true }<cr>", "Enabled" },
+                d = { "<cmd>lua require 'cmp'.setup{ enabled = false }<cr>", "Disabled" },
             },
+            -- Explorer
             E = {
                 name = "Explorer",
                 e = { "<cmd>Neotree toggle<cr>", "Reveal Explorer" },
@@ -105,6 +107,7 @@ return {
                 f = { "<cmd>Neotree float<cr>", "Floating Explorer" },
                 d = { open_neotree_to_dir, "Open Neotree to Directory" },
             },
+            -- Find
             F = {
                 name = "Find",
                 f = {
@@ -113,6 +116,7 @@ return {
                 },
                 t = { "<cmd>Telescope live_grep theme=ivy<CR>", "Find Text" },
             },
+            -- Git
             G = {
                 name = "Git",
                 g = { "<cmd>LazyGit<CR>", "Lazygit" },
