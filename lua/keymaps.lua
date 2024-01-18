e52c50d85d5fa16a1fe2ed93e0a91814ddc61b66
Junior Sundar <juniorsundar@gmail.com>
2024-01-18 23:46:42 +0400

    Complted Chapter

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -14,13 +14,14 @@ vim.keymap.set("n", "<C-h>", "<C-w>h", opts)
 vim.keymap.set("n", "<C-j>", "<C-w>j", opts)
 vim.keymap.set("n", "<C-k>", "<C-w>k", opts)
 vim.keymap.set("n", "<C-l>", "<C-w>l", opts)
-
 -- Resize with arrows
 -- delta: 2 lines
-vim.keymap.set("n", "<C-Up>", ":resize -2<CR>", opts)
-vim.keymap.set("n", "<C-Down>", ":resize +2<CR>", opts)
-vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>", opts)
-vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+vim.keymap.set("n", "<C-A-Up>", ":resize -2<CR>", opts)
+vim.keymap.set("n", "<C-A-Down>", ":resize +2<CR>", opts)
+vim.keymap.set("n", "<C-A-Left>", ":vertical resize -2<CR>", opts)
+vim.keymap.set("n", "<C-A-Right>", ":vertical resize +2<CR>", opts)
+vim.keymap.set("n", "<C-Right>", "w", opts)
+vim.keymap.set("n", "<C-Left>", "b", opts)
 
 -----------------
 -- Visual mode --
